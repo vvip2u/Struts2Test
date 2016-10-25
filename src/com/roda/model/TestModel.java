4c2dac9ab6cbf500a28c147af4901b9bf151c38f
Administrator <Administrator@JK-PC>
2016-10-26 02:14:51 +0800

    add config files

---
+++ src/com/roda/model/TestModel.java
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2013, 2016 JK Young Entrepreneur Club. All rights reserved. 
+ * We will help people who want to help themselves.
+ *******************************************************************************/
+package com.roda.model;
+
+/**
+ * 
+ * @author <a href="mailto:84454002@qq.com">金年康</a>
+ * @since 2016年10月26日
+ */
+public class TestModel {
+	
+
+	
+}
